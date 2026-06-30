0057034801d838b9382aedef134f326d9b2c4943
latifaashh <130272810+latifaashh@users.noreply.github.com>
2026-06-30 16:05:46 +0700

    Fix subheader text for consistency

--- mission_review.py
+++ mission_review.py
@@ -361,7 +361,7 @@ summary_submit['% Submit'] = (
     + '%'
 )
 
-st.subheader("📋ACTIVITY")
+st.subheader("📋Activity")
 
 summary_display = summary_submit.rename(
     columns={
@@ -426,7 +426,7 @@ score_summary = score_summary.sort_values(
 
 # Tampilkan tabel
 st.subheader(
-    "🏆SCORE ANSWER"
+    "🏆Score Answer"
 )
     
 st.dataframe(
